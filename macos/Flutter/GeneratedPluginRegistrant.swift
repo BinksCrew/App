4bc6cd8848748fefe2f93f561510bd82b651143e
cesardarizaleta <cesardarizaleta@gmail.com>
2026-01-12 11:24:55 -0400

    refactor: update dependencies and remove device_info_plus
    - Removed device_info_plus and its platform interface from pubspec.lock and GeneratedPluginRegistrant.swift.
    - Updated package_info_plus from 8.3.1 to 9.0.0.
    - Updated share_plus from 7.2.2 to 12.0.1.
    - Updated flutter_launcher_icons from 0.13.1 to 0.14.4.
    - Updated upgrader from 10.3.0 to 12.5.0.
    - Updated video_player from 2.8.6 to 2.9.1.
    - Updated shared_preferences from 2.5.3 to 2.5.4.
    - Bumped VERSION_CODE in version.properties from 20 to 27.
    - Removed deprecated output file name setting in build.gradle.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
 import file_selector_macos
 import local_auth_darwin
 import package_info_plus
@@ -15,7 +14,6 @@ import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
